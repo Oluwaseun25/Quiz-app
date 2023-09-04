1fb11ed0059194b2c6760413d4fb7c4b575d3266
Oluwaseun25 <Samuelawe842@gmail.com>
2023-09-04 12:09:56 +0100

    quiz app

---
+++ script.js
@@ -0,0 +1,24 @@
+// nb-56+%=
+const questions = [
+    {
+        question: "What does HTML stand for?",
+        answers: [
+            {text:"Hyper Transfer Markup Language", correct:false},
+            {text:"Hyperlink and Text Markup Language", correct:false},
+            {text:"Hyper Text Markup Language", correct:true},
+            {text:"Hyperlink Text Makeup Language", correct:false}
+        ]
+    },
+
+    {
+        question: "What is the result of 2 + 2 * 3?",
+        answers: [
+            {text:"4", correct:false},
+            {text:"8", correct:true},
+            {text:"7", correct:false},
+            {text:"10", correct:false}
+        ]
+    },
+
+
+]
