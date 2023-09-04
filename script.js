316f0c1d0c8facba99bbee932dfc1989d006a1db
Oluwaseun25 <Samuelawe842@gmail.com>
2023-09-04 12:23:08 +0100

    quiz app

--- script.js
+++ script.js
@@ -20,5 +20,57 @@ const questions = [
         ]
     },
 
+    {
+        question: "What is the largest planet in our solar system?",
+        answers: [
+            {text:"Mars", correct:false},
+            {text:"Earth", correct:false},
+            {text:"Venus", correct:false},
+            {text:"Jupiter", correct:true}
+        ]
+    },
+
+    {
+        question: "What is the capital of France?",
+        answers: [
+            {text:"Paris", correct:true},
+            {text:"Berlin", correct:false},
+            {text:"Madrid", correct:false},
+            {text:"London", correct:false}
+        ]
+    },
+
+    {
+        question: "In JavaScript, how do you declare a variable?",
+        answers: [
+            {text:"const", correct:false},
+            {text:"int", correct:false},
+            {text:"let", correct:true},
+            {text:"var", correct:false}
+        ]
+    },
+
+    {
+        question: " What is a closure in JavaScript?",
+        answers: [
+            {text:"A function that has no return statement.", correct:false},
+            {text:"A way to create private variables in a function.", correct:true},
+            {text:"A method for closing browser tabs.", correct:false},
+            {text:"A data type in JavaScript.", correct:false}
+        ]
+    },
+
+    {
+        question: "Which of the following is not a valid HTTP status code?",
+        answers: [
+            {text:"200 OK", correct:false},
+            {text:"404 Not Found", correct:false},
+            {text:" 500 Internal Server Error", correct:false},
+            {text:"300 Redirection", correct:true}
+        ]
+    },
+
+
 
 ]
+
